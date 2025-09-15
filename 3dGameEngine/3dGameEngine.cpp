ebdb812666347fd5e8797cab51a18c97371d1497
zaZZa564 <anus_bobra564@mail.ru>
2025-09-15 15:19:40 +0300

    fixed bugs 1.fAspectRatio value 2.Myltiplication matrix by vector

--- 3dGameEngine/3dGameEngine.cpp
+++ 3dGameEngine/3dGameEngine.cpp
@@ -33,10 +33,10 @@ private:
 	mat4x4 matProj;
 
 	//myltiply matrix by vector
-	void MyltiplyMatrixVector(vec3d& i, vec3d& o, mat4x4& mat) {
+	void MultiplyMatrixVector(vec3d &i, vec3d &o, mat4x4 &mat) {
 		o.x = i.x * mat.m[0][0] + i.y * mat.m[1][0] + i.z * mat.m[2][0] + mat.m[3][0];
 		o.y = i.x * mat.m[0][1] + i.y * mat.m[1][1] + i.z * mat.m[2][1] + mat.m[3][1];
-		o.y = i.x * mat.m[0][2] + i.y * mat.m[1][2] + i.z * mat.m[2][2] + mat.m[3][2];
+		o.z = i.x * mat.m[0][2] + i.y * mat.m[1][2] + i.z * mat.m[2][2] + mat.m[3][2];
 		float w = i.x * mat.m[0][3] + i.y * mat.m[1][3] + i.z * mat.m[2][3] + mat.m[3][3];
 
 		if (w != 0.0f) {
@@ -56,38 +56,37 @@ public:
 	bool OnUserCreate() override {
 		//coordinates with 'f' suffix to define float nums
 		meshCube.tris = {
-			//south
-			{0.0f, 0.0f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 1.0f, 0.0f},
-			{0.0f, 0.0f, 0.0f,   1.0f, 1.0f, 0.0f,   1.0f, 0.0f, 0.0f},
+			// SOUTH
+			{ 0.0f, 0.0f, 0.0f,    0.0f, 1.0f, 0.0f,    1.0f, 1.0f, 0.0f },
+			{ 0.0f, 0.0f, 0.0f,    1.0f, 1.0f, 0.0f,    1.0f, 0.0f, 0.0f },
 
-			//east
-			{1.0f, 0.0f, 0.0f,   1.0f, 1.0f, 0.0f,   1.0f, 1.0f, 1.0f},
-			{1.0f, 0.0f, 0.0f,   1.0f, 1.0f, 1.0f,   1.0f, 0.0f, 1.0f},
+			// EAST                                                      
+			{ 1.0f, 0.0f, 0.0f,    1.0f, 1.0f, 0.0f,    1.0f, 1.0f, 1.0f },
+			{ 1.0f, 0.0f, 0.0f,    1.0f, 1.0f, 1.0f,    1.0f, 0.0f, 1.0f },
 
-			//north
-			{1.0f, 0.0f, 1.0f,   1.0f, 1.0f, 1.0f,   0.0f, 1.0f, 1.0f},
-			{1.0f, 0.0f, 1.0f,   0.0f, 1.0f, 1.0f,   0.0f, 0.0f, 1.0f},
+			// NORTH                                                     
+			{ 1.0f, 0.0f, 1.0f,    1.0f, 1.0f, 1.0f,    0.0f, 1.0f, 1.0f },
+			{ 1.0f, 0.0f, 1.0f,    0.0f, 1.0f, 1.0f,    0.0f, 0.0f, 1.0f },
 
-			//west
-			{0.0f, 0.0f, 1.0f,   0.0f, 1.0f, 1.0f,   0.0f, 1.0f, 0.0f},
-			{0.0f, 0.0f, 1.0f,   0.0f, 1.0f, 0.0f,   0.0f, 0.0f, 0.0f},
+			// WEST                                                      
+			{ 0.0f, 0.0f, 1.0f,    0.0f, 1.0f, 1.0f,    0.0f, 1.0f, 0.0f },
+			{ 0.0f, 0.0f, 1.0f,    0.0f, 1.0f, 0.0f,    0.0f, 0.0f, 0.0f },
 
-			//top
-			{0.0f, 1.0f, 0.0f,   0.0f, 1.0f, 1.0f,   1.0f, 1.0f, 1.0f},
-			{0.0f, 1.0f, 0.0f,   1.0f, 1.0f, 1.0f,   1.0f, 1.0f, 0.0f},
+			// TOP                                                       
+			{ 0.0f, 1.0f, 0.0f,    0.0f, 1.0f, 1.0f,    1.0f, 1.0f, 1.0f },
+			{ 0.0f, 1.0f, 0.0f,    1.0f, 1.0f, 1.0f,    1.0f, 1.0f, 0.0f },
 
-			//bottom
-			{1.0f, 0.0f, 1.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, 0.0f},
-			{1.0f, 0.0f, 1.0f,   0.0f, 0.0f, 0.0f,   1.0f, 0.0f, 0.0f},
-
+			// BOTTOM                                                    
+			{ 1.0f, 0.0f, 1.0f,    0.0f, 0.0f, 1.0f,    0.0f, 0.0f, 0.0f },
+			{ 1.0f, 0.0f, 1.0f,    0.0f, 0.0f, 0.0f,    1.0f, 0.0f, 0.0f },
 		};
 
 		//projection matrix
 		float fNear = 0.1f;
 		float fFar = 1000.0f;
 		float fFov = 90.0f;
-		float fAspectRatio = (float)ScreenHeight() / (float)ScreenHeight();
-		float fFovRad = 1.0f / tanf(fFov * 0.5f / 180.f * PI); //radians
+		float fAspectRatio = (float)ScreenHeight() / (float)ScreenWidth();
+		float fFovRad = 1.0f / tanf(fFov * 0.5f / 180.0f * 3.14159f);
 
 		matProj.m[0][0] = fAspectRatio * fFovRad;
 		matProj.m[1][1] = fFovRad;
@@ -103,23 +102,32 @@ public:
 		Fill(0, 0, ScreenWidth(), ScreenHeight(), PIXEL_SOLID, FG_BLACK);
 
 		//draw triangles
-		for (auto &tri : meshCube.tris) {
-			triangle triProjected;
-			MyltiplyMatrixVector(tri.p[0], triProjected.p[0], matProj);
-			MyltiplyMatrixVector(tri.p[1], triProjected.p[1], matProj);
-			MyltiplyMatrixVector(tri.p[2], triProjected.p[2], matProj);
+		for (auto tri : meshCube.tris) {
+			triangle triProjected, triTranslated;
+
+			/*
+			triTranslated = tri;
+			triTranslated.p[0].z = tri.p[0].z + 3.0f;
+			triTranslated.p[1].z = tri.p[1].z + 3.0f;
+			triTranslated.p[2].z = tri.p[2].z + 3.0f;
+			*/
+
+			MultiplyMatrixVector(tri.p[0], triProjected.p[0], matProj);
+			MultiplyMatrixVector(tri.p[1], triProjected.p[1], matProj);
+			MultiplyMatrixVector(tri.p[2], triProjected.p[2], matProj);
 
 			//scale into view
+			
 			triProjected.p[0].x += 1.0f; triProjected.p[0].y += 1.0f;
 			triProjected.p[1].x += 1.0f; triProjected.p[1].y += 1.0f;
 			triProjected.p[2].x += 1.0f; triProjected.p[2].y += 1.0f;
 
 			triProjected.p[0].x *= 0.5f * (float)ScreenWidth();
-			triProjected.p[0].y *= 0.5f * (float)ScreenWidth();
+			triProjected.p[0].y *= 0.5f * (float)ScreenHeight();
 			triProjected.p[1].x *= 0.5f * (float)ScreenWidth();
-			triProjected.p[1].y *= 0.5f * (float)ScreenWidth();
+			triProjected.p[1].y *= 0.5f * (float)ScreenHeight();
 			triProjected.p[2].x *= 0.5f * (float)ScreenWidth();
-			triProjected.p[2].y *= 0.5f * (float)ScreenWidth();
+			triProjected.p[2].y *= 0.5f * (float)ScreenHeight();
 
 			//draw triangles
 			DrawTriangle(triProjected.p[0].x, triProjected.p[0].y,
